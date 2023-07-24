b532b951dc668371a55ce75db3040b776d57bbf3
Marhnie <sunijun97@gmail.com>
2023-07-24 15:21:15 +0900

    수정

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+[tableau]
+token_name = "streamlit-app"
+token_secret = "Jfl3hZXoRmeLx8S7RldI0w==:Y4SmsTWRGvbOktQfX6yTtb0MjtcuSmZQ"
+server_url = "https://prod-apnortheast-a.online.tableau.com"
+site_id = "marhnie"
